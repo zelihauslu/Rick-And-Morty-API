6397a8b0385362726251f721c144f57bb8d2f6ec
zelihauslu <zelusluz@gmail.com>
2023-03-03 00:24:57 +0300

    Project created

---
+++ Rick And Morty API/Rick And Morty API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick And Morty API
+//
+//  Created by Zeliha Uslu on 3.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
