fab8222cc50d47d260a4187e0b4a0d7424805faf
zelihauslu <zelusluz@gmail.com>
2023-03-03 12:05:43 +0300

    Update TabView

--- Rick And Morty API/Controllers /Core/RMTabViewController.swift
+++ Rick And Morty API/Controllers /Core/RMTabViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class RMTabViewController: UITabBarController {
-
+// it can not be subclass
+final class RMTabViewController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setUpTabs()
+        view.backgroundColor = .white
     }
-
-
+    
+    private func setUpTabs(){
+        let charactersVc = RMCharactersViewController()
+        let locationsVc = RMLocationsViewController()
+        let episodesVc = RMEpisodesViewController()
+        let settingsVc = RMSettingsViewController()
+        
+        charactersVc.title = "Characters"
+        locationsVc.title = "Locations"
+        episodesVc.title = "Episodes"
+        settingsVc.title = "Settings"
+        
+        let nav1 = UINavigationController(rootViewController: charactersVc)
+        let nav2 = UINavigationController(rootViewController: locationsVc)
+        let nav3 = UINavigationController(rootViewController: episodesVc)
+        let nav4 = UINavigationController(rootViewController: settingsVc)
+        
+        setViewControllers(
+            [nav1, nav2, nav3 ,nav4],
+            animated: true)
+    }
 }
+
 
