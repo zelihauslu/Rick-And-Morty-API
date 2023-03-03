785e812d4a4fc5a286ea459c99d9ed7b962246af
zelihauslu <zelusluz@gmail.com>
2023-03-03 16:08:09 +0300

    RMEpisode models added

--- Rick And Morty API/Models /RMEpisode.swift
+++ Rick And Morty API/Models /RMEpisode.swift
@@ -8,3 +8,16 @@
 import Foundation
 
 // Model is a basicallly a data type an object that represents some data it could be from an API it could just be some local objects
+
+struct RMEpisode : Codable{
+    let id: Int
+    let name: String
+    let air_date: String
+    let episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+}
+
+
+
