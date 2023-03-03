653a091bc045b6819a7d219fcf1c0f54676757b2
zelihauslu <zelusluz@gmail.com>
2023-03-03 11:43:40 +0300

    Create RMTabViewController

---
+++ Rick And Morty API/Controllers /Core/RMTabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick And Morty API
+//
+//  Created by Zeliha Uslu on 3.03.2023.
+//
+
+import UIKit
+
+class RMTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
